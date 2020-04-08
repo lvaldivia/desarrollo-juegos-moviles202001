fb0c455978e64393869923a99559226b2d48601a
luis <valdivialuis1989@gmail.com>
2020-04-07 22:25:50 -0500

    platform

--- Clase05/js/states/Game.js
+++ Clase05/js/states/Game.js
@@ -3,6 +3,15 @@ Game = function(game){}
 Game.prototype = {
 	create:function(){
 		this.levelData =  JSON.parse(this.cache.getText("level"));
-		console.log(this.levelData);
+		console.log(this.levelData.platformData);
+		this.platforms = this.game.add.group();
+		this.levelData.platformData.forEach(this.createPlatform,this);
+	},
+	createPlatform:function(element){
+		//primera forma
+		let platform = this.game.add.sprite(element.x,element.y,'platform');
+		this.platforms.add(platform);
+
+		//segunda forma
 	}
 }
