7fe18c9767aeeaa19869818b009181656b55577d
luis <valdivialuis1989@gmail.com>
2020-05-08 20:38:28 -0500

    game

--- Clase12/js/states/Game.js
+++ Clase12/js/states/Game.js
@@ -3,7 +3,7 @@ Game = function(game){}
 Game.prototype = {
 	init:function(currentLevel){
 		this.currentLevel = currentLevel ? currentLevel : "level1";
-		this.MAX_DISTANCE_SHOOT = 190;
+		this.MAX_DISTANCE_SHOOT = 300;
 		this.MAX_SPEED_SHOT = 1100;
 		this.SHOT_FACTOR = 12;
 		this.KILL_DIFF = 25;
@@ -39,11 +39,16 @@ Game.prototype = {
 		this.pole = this.game.add.sprite(180,500,'pole');
 		this.pole.anchor.setTo(0.5,0);
 
+		this.isChickenReady = false;
+		this.isPreparingShot = false;
 		this.game.input.onDown.add(this.prepareShot,this);
 		this.setupChicken();
 	},
 	prepareShot:function(){
-
+		if(this.isChickenReady){
+			this.isPreparingShot = true;
+			this.isChickenReady = false;
+		}
 	},
 	loadLevel:function(){
 		this.levelData = JSON.parse(this.game.cache.getText(this.currentLevel));
@@ -59,9 +64,20 @@ Game.prototype = {
 		this.enemies.add(enemy);
 		enemy.body.setCollisionGroup(this.enemiesCollisionGroup);
 		enemy.body.collides([this.blocksCollisionGroup,this.chickenCollisionGroup,this.enemiesCollisionGroup]);
-		enemy.body.onBeginContact.add(this.hitEnemy,enemy);
+		enemy.body.onBeginContact.add(this.hitEnemy,this);
 	},
-	hitEnemy:function(){
+	hitEnemy:function(bodyA,bodyB,shapeA,shapeB,equation){
+		
+		let  velocityDiff = Phaser.Point.distance(
+			new Phaser.Point(equation[0].bodyA.velocity[0],equation[0].bodyA.velocity[1]),
+			new Phaser.Point(equation[0].bodyB.velocity[0],equation[0].bodyB.velocity[1]),
+		);
+		if(velocityDiff > this.KILL_DIFF){
+			this.updateDeadCount();	
+		}
+		
+	},
+	updateDeadCount:function(){
 		
 	},
 	createBlock:function(data){
@@ -71,7 +87,6 @@ Game.prototype = {
 		block.body.setCollisionGroup(this.blocksCollisionGroup);
 		let collision = [this.blocksCollisionGroup,this.enemiesCollisionGroup,this.chickenCollisionGroup];
 		block.body.collides(collision);
-
 	},
 	setupChicken:function(){
 		this.chicken = this.game.add.sprite(this.pole.x,this.pole.y,'chicken');
@@ -86,5 +101,57 @@ Game.prototype = {
 			this.chickenHUD.create(this.game.width- 100 - i*80,30,'chicken');
 			i++;
 		}
+	},
+	update: function(){
+		if(this.isPreparingShot){
+			this.chicken.x = this.game.input.activePointer.position.x;
+			this.chicken.y = this.game.input.activePointer.position.y;
+
+			let distance = Phaser.Point.distance(this.chicken.position,this.pole.position);
+			if(distance > this.MAX_DISTANCE_SHOOT){
+				this.isPreparingShot = false;
+				this.isChickenReady = true;
+				this.chicken.x = this.pole.x;
+				this.chicken.y = this.pole.y;
+			}
+			if(this.game.input.activePointer.isUp){
+				this.isPreparingShot = false;
+				this.throwChicken();
+			}
+		}
+	},
+	throwChicken:function(){
+		this.game.physics.p2.enable(this.chicken);
+		this.chicken.body.setCollisionGroup(this.chickenCollisionGroup);
+		this.chicken.body.collides([this.blocksCollisionGroup,this.enemiesCollisionGroup,this.chickenCollisionGroup]);
+
+		let diff = Phaser.Point.subtract(this.pole.position,this.chicken.position);
+
+		this.chicken.body.velocity.x = Math.abs(diff.x*this.SHOT_FACTOR) 
+						/ (diff.x *this.SHOT_FACTOR) * Math.min(Math.abs(diff.x*this.SHOT_FACTOR),this.MAX_SPEED_SHOT);
+
+		this.chicken.body.velocity.y = Math.abs(diff.y*this.SHOT_FACTOR) 
+						/ (diff.y *this.SHOT_FACTOR) * Math.min(Math.abs(diff.y*this.SHOT_FACTOR),this.MAX_SPEED_SHOT);
+
+		this.endTurn();
+	},
+	endTurn:function(){
+		this.numChicken--;
+
+		this.game.time.events.add(3*Phaser.Timer.SECOND,function(){
+			this.chicken.kill();
+
+			this.game.time.events.add(Phaser.Timer.SECOND,function(){
+				if(this.numChicken>0){
+					this.setupChicken();
+				}else{
+					this.refreshStats();
+					this.gameOver();
+				}
+			},this)
+		},this);
+	},
+	gameOver:function(){
+		//GOGAMEOVR
 	}
 }
